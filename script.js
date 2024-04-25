5e0b6a48d6888430b229db629016e51c7311cb8b
msk2924 <146979324+msk2924@users.noreply.github.com>
2024-04-25 20:50:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    // You can add your form submission logic here, like sending an email or storing data.
+    alert('Your message has been sent!');
+});
